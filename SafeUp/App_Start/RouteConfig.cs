039ddc7e1703fe85320189eeb5adba2b6a3a17b6
emgie123 <garbacz.m@hotmail.com>
2015-03-09 11:00:45 +0100

    aaa

--- SafeUp/App_Start/RouteConfig.cs
+++ SafeUp/App_Start/RouteConfig.cs
@@ -13,7 +13,6 @@ namespace SafeUp
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
