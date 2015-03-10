8aa25d666de923360599a44e41cd1902515a8f98
mg <garbacz.m@hotmail.com>
2015-03-10 01:36:25 +0100

    progress

---
+++ SafeUp/App_Start/MyCustomViewLocations.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace SafeUp.App_Start
+{
+    public class MyCustomViewLocations : RazorViewEngine
+    {
+        public MyCustomViewLocations()
+        {
+            ViewLocationFormats = new string[]
+            {
+                "~/Views/{1}/{0}.cshtml",
+                "~/Views/{1}/{0}"
+            };
+            MasterLocationFormats = ViewLocationFormats;
+            PartialViewLocationFormats = ViewLocationFormats;
+        }
+
+       
+    }
+}
