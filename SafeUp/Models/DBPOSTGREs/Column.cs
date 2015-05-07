08dd088b9bfcc4120e57329f74274cc694ca8e9b
robson081192 <robson081192@gmail.com>
2015-05-07 12:27:19 +0200

    sdvdfvdf

--- SafeUp/Models/DBPOSTGREs/Column.cs
+++ SafeUp/Models/DBPOSTGREs/Column.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using SafeUp.Models.DBPOSTGREs.Interfaces;
 
 namespace SafeUp.Models.DBPOSTGREs
 {
-    public class Column
+    public class Column<T> : IColumn<T>
     {
+        public Type ColumnType { get; set; }
+        public T ColumnyValue { get; set; }
+        public string ColumnName { get; set; }
     }
 }
---
+++ SafeUp/Models/SafeUpModels/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SafeUp.Models.DBPOSTGREs;
+using SafeUp.Models.DBPOSTGREs.Interfaces;
+
+namespace SafeUp.Models.DbModels
+{
+    public class User : Row
+    {
+        public Column<string> Login { get; set; }
+        public Column<string> Password { get; set; }
+        public Column<DateTime> CreatedOn { get; set; }
+        public Column<AccountType> AccountType { get; set; } 
+    }
+}
