21253bdb3c9b73e9861d63fa7017538d2cf202a5
mg <garbacz.m@hotmail.com>
2015-05-07 12:10:14 +0200

    ffd

---
+++ SafeUp/Models/DBPOSTGREs/Row.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SafeUp.Models.DBPOSTGREs.Interfaces;
+
+namespace SafeUp.Models.DBPOSTGREs
+{
+    public class Row : IRow
+    {
+        public int RowId { get; set; }
+        public List<IColumn<object>> Columns { get; set; }
+    }
+}
