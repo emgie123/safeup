01460370b57ab1695dc9b10cab6b91524beb7850
mg <garbacz.m@hotmail.com>
2015-05-07 12:35:42 +0200

    repozytorium sourcee tree

--- SafeUp/Models/SafeUpModels/TestModel.cs
+++ SafeUp/Models/SafeUpModels/TestModel.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using SafeUp.Models.DBAL.Interfaces;
-using SafeUp.Models.DBAL.Abstraction;
+using SafeUp.Models.DBPOSTGREs;
 
 namespace SafeUp.Models.DbModels
 {
@@ -15,7 +14,7 @@ namespace SafeUp.Models.DbModels
             //  ogólnie dodawany tutaj do słownika za każdym razem klucz (key) jest trochę chujowy, bo i tak nazwa i warość danej kolumny
             //  przechowywane są w wartości słownika (value). musimy tak coś pomyślę, by zastąpić klucz czymś bardziej uniwersalnym albo 
             //  lepszym bo póki co powtarzamy dane.
-            this.TableName = "test";
+          //  this.TableName = "test";
 
             //ogólnie dodawanie danych do tabeli już działa, ale wydaje mi się, że to jest trochę chujowe jak musimy w modelu dodawać elementy
             //do słownika po czym dopiero zatwierdzać inserta. Jutro musimy to obmyśleć. Aha nie robiłem autoincrementa id bo nie wiem czy
@@ -23,10 +22,10 @@ namespace SafeUp.Models.DbModels
             //this.Row.Add("id", new Column<object>("id", 666));
             //this.Row.Add("nazwa", new Column<object>("nazwa", "nowy test"));
             //this.Row.Add("opis", new Column<object>("opis", "nowe blebleble"));
-            this.SetValue("id",123);
-            this.SetValue("nazwa", "sorounded test");
-            this.SetValue("opis", "insert poprawiony delimiter");
-            this.SetWhere("id");
+           // this.SetValue("id",123);
+           // this.SetValue("nazwa", "sorounded test");
+           // this.SetValue("opis", "insert poprawiony delimiter");
+           // this.SetWhere("id");
 
             
         }
