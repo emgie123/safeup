f61b7b38fa10bb120d905d6d61dd294f446d6469
robson081192 <robson081192@gmail.com>
2015-05-12 00:14:55 +0200

    dfvdfv

--- SafeUp/Controllers/MyFilesController.cs
+++ SafeUp/Controllers/MyFilesController.cs
@@ -24,23 +24,20 @@ namespace SafeUp.Controllers
             {
                 files = handler.GetFilesModel();
             }
-            //foreach (var file in files.Rows)
-            //{
-            //    if (!file.Value.Columns["owner"].ColumnyValue.Equals(Session["ID"]))
-            //    {
-            //        continue;
-            //    }
-            //    userFiles.Add(new File()
-            //    {
-            //        ID = new Column<int>(){ColumnName = "ID", ColumnyValue = file.Key},
-            //        Name = new Column<string>() { ColumnName = "Name", ColumnyValue = file.Value.Columns["name"].ColumnyValue.ToString()},
-            //        Path = new Column<string>() { ColumnName = file.Value.Columns["path"].ColumnyValue.ToString()},
-            //        Owner = new Column<int>() { ColumnName = "Owner", ColumnyValue = (int)file.Value.Columns["owner"].ColumnyValue},
-            //        Size = new Column<double>() { ColumnName = "Size", ColumnyValue = (double)file.Value.Columns["size"].ColumnyValue},
-            //        Key = new Column<string>() { ColumnName = "Key", ColumnyValue = file.Value.Columns["key"].ColumnyValue.ToString()},
-            //        CreatedOn = new Column<DateTime>() { ColumnName = "CreatedOn", ColumnyValue = DateTime.Parse(file.Value.Columns["created_on"].ColumnyValue.ToString()) }                    
-            //    });
-            //}
+            foreach (var file in files.Rows)
+            {
+                if(!file.Value.Owner.Equals(Session["ID"]))
+                {
+                    continue;
+                }
+                userFiles.Add(new File()
+                {
+                    ID = file.Key,
+                    Name = file.Value.Name,
+                    CreatedOn = file.Value.CreatedOn,
+                    Size = file.Value.Size
+                });
+            }
            return PartialView("~/Views/Partials/LoggedIn/Files/MyFilesPartial.cshtml", userFiles);
  
         }
